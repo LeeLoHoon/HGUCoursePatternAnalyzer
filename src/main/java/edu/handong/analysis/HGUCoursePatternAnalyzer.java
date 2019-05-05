21cb458118fedca52577a4c43c73660d100d0835
wldn5 <wldn5@DESKTOP-LQ4BBPD>
2019-05-05 17:09:00 +0900

    save point

--- src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
+++ src/main/java/edu/handong/analysis/HGUCoursePatternAnalyzer.java
@@ -5,7 +5,7 @@ import edu.handong.analysis.datamodel.Student;
 
 public class HGUCoursePatternAnalyzer {
 	
-	String[] lines = {	"1999-1, JC Nam, Java Programming",
+	private String[] lines = {	"1999-1, JC Nam, Java Programming",
 						"1999-2, JC Nam, Programming Language Theory",
 						"1999-1, JC Nam, Data Structures",
 						"2001-1, JC Nam, Database Systems",
@@ -19,10 +19,10 @@ public class HGUCoursePatternAnalyzer {
 						"2019-1, SJ Kim, Algorithm Analysis",
 						};
 
-	int numOfStudents;
-	int numOfCourses;
-	Student[] students;
-	Course[] courses;
+	private int numOfStudents;
+	private int numOfCourses;
+	private Student[] students; //instance
+	private Course[] courses;
 	
 	/**
 	 * This method runs our analysis logic to get the list of student and course names from lines.
@@ -33,14 +33,14 @@ public class HGUCoursePatternAnalyzer {
 		numOfStudents = Integer.parseInt(args[0]);
 		numOfCourses = Integer.parseInt(args[1]);
 	
-		students = initiateStudentArrayFromLines(lines);
+		students = initiateStudentArrayFromLines(lines); //lines 에서 중복안되게 학생만 뽑아내서 넣기
 		
 		System.out.println("Number of All Students: " + numOfStudents);
 		for(Student student: students) {
 			System.out.println(student.getName());
 		}
 		
-		courses = initiateCourseArrayFromLines(lines);
+		courses = initiateCourseArrayFromLines(lines); //lines 에서 중복안되게 코스만 뽑아내서 넣기
 		System.out.println("Number of All Courses: " + numOfCourses);
 		for(Course course: courses) {
 			System.out.println(course.getCourseName());
@@ -54,11 +54,20 @@ public class HGUCoursePatternAnalyzer {
 	 * @return
 	 */
 	private Student[] initiateStudentArrayFromLines(String[] lines) {
-		
+		int i=0,j=0;
+		while(lines[i]!=null) {
+			Student elementStudent = new Student(lines[i].trim().split(",")[1]);
+			if (studentExist(students,elementStudent)) {
+				students[j]=elementStudent;
+				System.out.println(elementStudent);
+				j++;
+				i++;}
+			else i++;}
+		//trim(), split() 써서 lines 쪼개고 거기서 두번쨰 index들을  studentExist를 써서 비교해가며 중복안되게 넣기
 		// TODO: implement this method
 		
 		
-		return null;
+		return students;
 	}
 
 	/**
@@ -68,10 +77,12 @@ public class HGUCoursePatternAnalyzer {
 	 * @return boolean
 	 */
 	private boolean studentExist(Student[] students, Student student) {
-		
+		int i=0;
+		while(students[i]!=null)
+			if(student==students[i])
+				return false;
+		return true;
 		// TODO: implement this method
-
-		return false;
 	}
 	
 	/**
@@ -80,10 +91,17 @@ public class HGUCoursePatternAnalyzer {
 	 * @return
 	 */
 	private Course[] initiateCourseArrayFromLines(String[] lines) {
-		
+		int i=0,j=0;
+		while(lines[i]!=null) {
+			Course elementCourse = new Course(lines[i].trim().split(",")[2]);
+			if (courseExist(courses,elementCourse)) {
+				courses[j]=elementCourse;
+				j++;
+				i++;}
+			else i++;}		
 		// TODO: implement this method
 		
-		return null;
+		return courses;
 	}
 
 	/**
@@ -93,10 +111,13 @@ public class HGUCoursePatternAnalyzer {
 	 * @return boolean
 	 */
 	private boolean courseExist(Course[] courses, Course course) {
+		int i=0;
+		while(courses[i]!=null)
+			if(course==courses[i])
+				return false;
+		return true;
 		
 		// TODO: implement this method
-
-		return false;
 	}
 
 }
